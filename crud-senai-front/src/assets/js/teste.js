d74468ffad680f21f490bfb884cf1a7aa9d336a0
unknown <t.rangel@labcetec.net>
2026-03-02 15:53:50 -0300

    Primeiro commit. Subindo o front-end para o repositório

---
+++ crud-senai-front/src/assets/js/teste.js
@@ -0,0 +1,19 @@
+const cores = Object.freeze({
+    RED: "red",
+    BLUE: "blue"
+});
+
+console.log(cores.RED);
+console.log(cores.BLUE);
+
+switch (cores) {
+    case RED:
+        
+        break;
+    case BLUE:
+        
+        break;
+
+    default:
+        break;
+}
